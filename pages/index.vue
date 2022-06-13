ef1cf910e7940c95e14e9800c2a47b0cc7045cf5
Nazneen Oomatia <naz.chini@gmail.com>
2022-06-13 17:46:11 +0200

    Apply placeholders with  object and setup error component

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,59 @@
 // latest articles about Nuxt will be listed here
 <template>
   <div class="page-wrapper">
-    <div class="article-cards-wrapper">
-      <article-card-block
-        v-for="(article, i) in articles"
-        :key="article.id"
-        v-observe-visibility="
-          i === articles.length - 1 ? lazyLoadArticles : false
-        "
-        :article="article"
-        class="article-card-block"
-      />
-    </div>
+    <template v-if="$fetchState.pending && !articles.length">
+      <div class="article-cards-wrapper">
+        <content-placeholders
+          v-for="p in 30"
+          :key="p"
+          rounded
+          class="article-card-block"
+        >
+          <content-placeholders-img />
+          <content-placeholders-text :lines="3" />
+        </content-placeholders>
+      </div>
+    </template>
+    <template v-else-if="$fetchState.error">
+      <inline-error-block :error="$fetchState.error" />
+    </template>
+    <template v-else>
+      <div class="article-cards-wrapper">
+        <article-card-block
+          v-for="(article, i) in articles"
+          :key="article.id"
+          v-observe-visibility="
+            i === articles.length - 1 ? lazyLoadArticles : false
+          "
+          :article="article"
+          class="article-card-block"
+        />
+      </div>
+    </template>
+    <template v-if="$fetchState.pending && articles.length">
+      <div class="article-cards-wrapper">
+        <content-placeholders
+          v-for="p in 30"
+          :key="p"
+          rounded
+          class="article-card-block"
+        >
+          <content-placeholders-img />
+          <content-placeholders-text :lines="3" />
+        </content-placeholders>
+      </div>
+    </template>
   </div>
 </template>
 
 <script>
 import ArticleCardBlock from "@/components/blocks/ArticleCardBlock";
+import InlineErrorBlock from "@/components/blocks/InlineErrorBlock";
 
 export default {
   components: {
     ArticleCardBlock,
+    InlineErrorBlock,
   },
   data() {
     return {
@@ -32,9 +65,12 @@ export default {
     lazyLoadArticles(isVisible) {
       if (isVisible) {
         if (this.currentPage < 5) {
-          this.currentPage++
-          this.$fetch()
+          this.currentPage++;
+          this.$fetch();
         }
+      }
+    },
+  },
   async fetch() {
     const articles = await fetch(
       `https://dev.to/api/articles?tag=nuxt&state=rising&page=${this.currentPage}`
