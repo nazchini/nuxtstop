2eeb46b6264fa1deb32ef08d247b64cab4310dc5
Nazneen Oomatia <naz.chini@gmail.com>
2022-06-13 21:48:14 +0200

    Setup header and footer to all pages/views

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <the-header />
+    <Nuxt keep-alive />
+    <the-footer />
+  </div>
+</template>
+
+<script>
+import TheHeader from "@/components/TheHeader";
+import TheFooter from "@/components/TheFooter";
+
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+};
+</script>
